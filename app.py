f092ea9e9d7ebc8b29c054d04c5c4cb3716839c0
GolfPoolTracking <51084242+GolfPoolTracking@users.noreply.github.com>
2026-05-25 16:38:52 +0100

    change score options order to have all square in the middle instead of at the beginning

--- app.py
+++ app.py
@@ -140,7 +140,7 @@ def generate_pairing_html(p, view_mode="public", hide_names=False, reveal_time=N
 
 # --- LIST DEFINITIONS ---
 HOLE_OPTIONS = [str(i) for i in range(1, 19)] + [f"Extra Hole {i}" for i in range(1, 10)]
-SCORE_OPTIONS = ["All Square"] + [f"{i} Up" for i in range(1, 10)] + [f"{i} Down" for i in range(1, 10)]
+SCORE_OPTIONS =  [f"{i} Up" for i in range(1, 10)] + ["All Square"] + [f"{i} Down" for i in range(1, 10)]
 VENUE_OPTIONS = ["Home", "Away"]
 CATEGORY_OPTIONS = ["Mens", "Womens", "Boys", "Mixed"]
 
